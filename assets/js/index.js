f88e5ea5c6c7e18ef2cfceb1e50060942c00134c
Viktoriia <freshitpte@gmail.com>
2024-05-04 18:22:04 +0300

    Add js basics + datatypes

---
+++ assets/js/index.js
@@ -0,0 +1,84 @@
+// JS - мова з динамічною (качина) типізацією
+
+// js виконується браузером (движком)
+
+// Стандарт ECMASctipt
+
+// Змінні
+
+// lowerCamelCase - для змінних/функцій/...
+// UpperCamelCase - класи
+// UPPER_SNAKE_CASE - константи (PI)
+
+// 1    a-z, A-Z, _, $
+// 2... a-z, A-Z, _, $, 0-9
+
+/*
+alert("Hello World 1");
+alert("Hello World 2");
+*/
+
+// let, const,
+// var - старий стиль
+// let age = 18;
+// alert(age);
+// age = 19;
+// alert(age);
+
+// const count = 10;
+// count = 11;
+// alert(count);
+
+// const PI = 3.14;
+// userName
+// user-name
+
+// number 0 1 -10 159, 15.88, -0, -Infinity, 0.5e2, NaN - not a number
+// let n1 = 9999999999999999999999999999999999999;
+// alert(n1);
+
+// bigint
+// let bigNumber = 999999999999999999999999n;
+// alert(bigNumber);
+
+// string
+// let userName = "Ivo";
+// alert(userName);
+
+// let phrase = "His name is 'Ivo'";
+// alert(phrase);
+// let someHtml = `
+//   <h1 style="color: red;">Head</h1>
+//   <p>Pharagraph.</p>
+// `;
+
+// document.write(someHtml);
+
+// boolean - true, false
+// let isEmail = true;
+// alert(isEmail);
+// стать
+// let gender = "male";
+// стать
+// let isMale = false; // true - ч, false - ж
+
+// Task: Створити 4 змінні:
+// для свого імені, email, віку та статі.
+// let name = "name007";
+// let email = "male@gmail.com";
+// let age = 23;
+// let isMale = true;
+// alert("Name: " + name); // 1 параметр
+// console.log("Name:", name, "\nEmail:", email); // кілька параметрів, кома ->' '
+
+// null
+// let a = null;
+// alert(a);
+
+// undefined
+// let b;
+// alert(b);
+
+// symbol
+
+// не примітив - object
