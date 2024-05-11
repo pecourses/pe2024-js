782822c2c01b606fe4dc0f3b6b65ef43728b9ea9
Viktoriia <freshitpte@gmail.com>
2024-05-11 16:26:42 +0300

    Add template literal

---
+++ assets/js/templateLiteral.js
@@ -0,0 +1,16 @@
+const greeting = "Hello";
+const name = "Test";
+
+console.log(greeting + " " + name + "!");
+const resultString = `${greeting} ${name}!`;
+console.log(resultString);
+
+const a = 1;
+const b = 10;
+console.log(a + b);
+console.log("a+b= :>> ", a + b);
+// 1 + 10 = 11
+// console.log(`a + b = ${result}`);
+
+const resultNumber = `${a} + ${b} = ${a + b}`;
+console.log(resultNumber);
