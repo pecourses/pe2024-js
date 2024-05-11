3feba46dcb0951634b15f66671c1b75caae5bf4d
Viktoriia <freshitpte@gmail.com>
2024-05-11 16:59:46 +0300

    Add ?:

--- assets/js/conditions.js
+++ assets/js/conditions.js
@@ -28,23 +28,39 @@ if (isEnoughMoney) {
   document.write(`<p>Операцію відхилено</p>`);
 }
 
-const isOnline = false;
+// const isOnline = true;
 
 // {} - Block область видимості
 
-let color = null;
-if (isOnline) {
-  color = "green";
-} else {
-  color = "red";
-}
+// let color = null;
+// let status = null;
 
-console.log(color);
+// if (isOnline) {
+//   color = "green";
+//   status = "online";
+// } else {
+//   color = "red";
+//   status = "offline";
+// }
 
-if (isOnline) {
-  document.write(`<p>User is <span style="color:${color};">online</span></p>`);
-} else {
-  document.write(
-    `<p>User is  <span style="color:${color};">offline</span></p>`
-  );
-}
+// // if (isOnline) {
+// //   document.write(`<p>User is <span style="color:${color};">online</span></p>`);
+// // } else {
+// //   document.write(
+// //     `<p>User is  <span style="color:${color};">offline</span></p>`
+// //   );
+// // }
+
+// document.write(`<p>User is <span style="color:${color};">${status}</span></p>`);
+
+// 3
+const isOnline = true;
+
+// умовна (тернатна) операція ?:
+const color = isOnline ? "green" : "red";
+const status = isOnline ? "online" : "offline";
+
+// Єдине джерело істини, DRY
+document.write(`<p>User is <span style="color:${color};">${status}</span></p>`);
+
+//
