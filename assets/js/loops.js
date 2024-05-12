2c61c9962bbaeadb56ed1dbe4bc23a8f06890093
Viktoriia <freshitpte@gmail.com>
2024-05-12 16:00:48 +0300

    Add while

---
+++ assets/js/loops.js
@@ -0,0 +1,57 @@
+// console.log("Пройти прямо ");
+// console.log("Повернути на 90 град наліво");
+// console.log("Пройти прямо ");
+// console.log("Повернути на 90 град наліво");
+// console.log("Пройти прямо ");
+// console.log("Повернути на 90 град наліво");
+// console.log("Пройти прямо ");
+// console.log("Повернути на 90 град наліво");
+
+// while (умова) { цикл з передумовою
+//   тіло циклу
+// }
+
+// змінна цикла
+// 1 прохід циклу - ітерація
+
+// let currentStep = 1;
+// const STEPS_COUNT = 4;
+
+// while (currentStep <= STEPS_COUNT) {
+//   // console.log(currentStep);
+//   console.log("Пройти прямо ");
+//   console.log("Повернути на 90 град наліво");
+//   currentStep++; //currentStep = currentStep + 1;
+// }
+// console.log("Next line");
+
+// Вивести номери сторінок з 10 до 20
+
+// let startPage = 10;
+// const END_PAGE = 20;
+
+// while (startPage <= END_PAGE) {
+//   console.log(startPage++);
+//   // startPage++;
+// }
+
+// Вивести тільки парні номери сторінок
+//  10-20: 10, 12, 14, ...
+// let currentPage = 10;
+// const END_PAGE = 20;
+
+// while (currentPage <= END_PAGE) {
+//   console.log("Current page: ", currentPage);
+//   currentPage += 2;
+// }
+
+// ВВести початкову і кінцеву сторінку і вивести парні номери
+// let currentPage = Number(prompt("Input start page"));
+// const endPage = Number(prompt("Input end page"));
+
+// while (currentPage <= endPage) {
+//   if (currentPage % 2 === 0) {
+//     console.log(currentPage);
+//   }
+//   currentPage += 1;
+// }
