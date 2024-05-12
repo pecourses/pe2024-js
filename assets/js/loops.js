e1a3fc729d7e902ae159d5d098fd3a0c5272b22a
Viktoriia <freshitpte@gmail.com>
2024-05-12 21:04:55 +0300

    Add some comments

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -14,47 +14,47 @@
 // змінна цикла
 // 1 прохід циклу - ітерація
 
-// let currentStep = 1;
-// const STEPS_COUNT = 4;
+let currentStep = 1;
+const STEPS_COUNT = 4;
 
-// while (currentStep <= STEPS_COUNT) {
-//   // console.log(currentStep);
-//   console.log("Пройти прямо ");
-//   console.log("Повернути на 90 град наліво");
-//   currentStep++; //currentStep = currentStep + 1;
-// }
-// console.log("Next line");
+while (currentStep <= STEPS_COUNT) {
+  // console.log(currentStep);
+  console.log("Пройти прямо ");
+  console.log("Повернути на 90 град наліво");
+  currentStep++; //currentStep = currentStep + 1;
+}
+console.log("Next line");
 
 // Вивести номери сторінок з 10 до 20
 
-// let startPage = 10;
-// const END_PAGE = 20;
+let startPage = 10;
+const END_PAGE = 20;
 
-// while (startPage <= END_PAGE) {
-//   console.log(startPage++);
-//   // startPage++;
-// }
+while (startPage <= END_PAGE) {
+  console.log(startPage++);
+  // startPage++;
+}
 
 // Вивести тільки парні номери сторінок
 //  10-20: 10, 12, 14, ...
-// let currentPage = 10;
-// const END_PAGE = 20;
+let currentPage = 10;
+const END_PAGE = 20;
 
-// while (currentPage <= END_PAGE) {
-//   console.log("Current page: ", currentPage);
-//   currentPage += 2;
-// }
+while (currentPage <= END_PAGE) {
+  console.log("Current page: ", currentPage);
+  currentPage += 2;
+}
 
 // ВВести початкову і кінцеву сторінку і вивести парні номери
-// let currentPage = Number(prompt("Input start page"));
-// const endPage = Number(prompt("Input end page"));
+let currentPage = Number(prompt("Input start page"));
+const endPage = Number(prompt("Input end page"));
 
-// while (currentPage <= endPage) {
-//   if (currentPage % 2 === 0) {
-//     console.log(currentPage);
-//   }
-//   currentPage += 1;
-// }
+while (currentPage <= endPage) {
+  if (currentPage % 2 === 0) {
+    console.log(currentPage);
+  }
+  currentPage += 1;
+}
 
 // ВВести число і, якщо воно коректне, вивести число в квадраті
 
@@ -63,14 +63,14 @@
 // 3 якщо нескінченне, то ввести заново - > step 2
 // 4 якщо скінченне, то піднести до квадрату
 
-// let inputNumber = Number(prompt("Input number"));
+let inputNumber = Number(prompt("Input number"));
 
-// // isFinite - false, if Infinity, NaN
-// while (!Number.isFinite(inputNumber)) {
-//   inputNumber = Number(prompt("Input number"));
-// }
+// isFinite - false, if Infinity, NaN
+while (!Number.isFinite(inputNumber)) {
+  inputNumber = Number(prompt("Input number"));
+}
 
-// console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
+console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
 
 // let inputNumber = Number(prompt("Input number"));
 // якщо є break або continue - спробувати переписати без них
@@ -86,26 +86,26 @@
 // do..while
 
 // do {
-//   // тіло циклу
+//   тіло циклу
 // } while (умова); // цикл з післяумовою
 
-// let inputNumber = null;
+let inputNumber = null;
 
-// do {
-//   inputNumber = Number(prompt("Input number"));
-// } while (!Number.isFinite(inputNumber));
+do {
+  inputNumber = Number(prompt("Input number"));
+} while (!Number.isFinite(inputNumber));
 
-// console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
+console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
 
 // Вивести числа з 50 до 40 do..while
 
-// let currentPage = 50;
-// const END_PAGE = 40;
+let currentPage = 50;
+const END_PAGE = 40;
 
-// do {
-//   console.log(`Current page ${currentPage}`);
-//   currentPage--;
-// } while (END_PAGE <= currentPage);
+do {
+  console.log(`Current page ${currentPage}`);
+  currentPage--;
+} while (END_PAGE <= currentPage);
 
 // Користувач має 3 спроби ввести пароль
 // Якщо пароль вірний, то вивести "Пароль вірний"
@@ -131,16 +131,16 @@
 //   console.log("Password is wrong");
 // }
 
-// const RIGHT_PASSWORD = "qwerty";
-// const TRY_LIMIT = 3;
-// let isPasswordCorrect = false;
-// let currentTry = 1;
+const RIGHT_PASSWORD = "qwerty";
+const TRY_LIMIT = 3;
+let isPasswordCorrect = false;
+let currentTry = 1;
 
-// do {
-//   isPasswordCorrect = prompt("Input password") === RIGHT_PASSWORD;
-// } while (++currentTry <= TRY_LIMIT && !isPasswordCorrect);
+do {
+  isPasswordCorrect = prompt("Input password") === RIGHT_PASSWORD;
+} while (++currentTry <= TRY_LIMIT && !isPasswordCorrect);
 
-// console.log(`Password is ${isPasswordCorrect ? "correct" : "wrong"}`);
+console.log(`Password is ${isPasswordCorrect ? "correct" : "wrong"}`);
 
 // якщо тіло циклу має виконатися хоч 1 раз, то do..while
 // інакше while
@@ -150,12 +150,13 @@
 //   тіло циклу
 // }
 
-// const stopPage = 10;
+const stopPage = 10;
 
-// for (let startPage = 1; startPage <= stopPage; startPage++) {
-//   console.log(startPage);
-// }
+for (let startPage = 1; startPage <= stopPage; startPage++) {
+  console.log(startPage);
+}
 
+// =
 // let startPage = 1;
 // while (startPage <= stopPage) {
 //   body;
@@ -164,19 +165,19 @@
 
 // вивести всі (непарні) значення з 101 до 91 (101, 99, 97, ...)
 
-// const END_NUM = 91;
+const END_NUM = 91;
 
-// for (let startNum = 101; startNum >= END_NUM; startNum -= 2) {
-//   console.log(startNum);
-// }
+for (let startNum = 101; startNum >= END_NUM; startNum -= 2) {
+  console.log(startNum);
+}
 
-// let startNumber = 101;
-// let endNumber = 91;
-// for (let i = startNumber; i >= endNumber; i--) {
-//   if (i % 2 !== 0) {
-//     console.log(i);
-//   }
-// }
+let startNumber = 101;
+let endNumber = 91;
+for (let i = startNumber; i >= endNumber; i--) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
 
 // 0 (ініціалізація лічильника) виконується 1 раз до циклу
 // 1 (умова продовження циклу)  якщо умова істинна, то виконуємо тіло циклу 2, інакше вихід
@@ -189,14 +190,14 @@
 
 // Знайти суму чисел від 1 до 20
 // 1+2+3+4+....+20
-// const startNumber = 1;
-// const endNumber = 3;
-// let sum = 0; // 0+1+2+... // accumulator
+const startNumber = 1;
+const endNumber = 3;
+let sum = 0; // 0+1+2+... // accumulator
 
-// for (let i = startNumber; i <= endNumber; i++) {
-//   sum = sum + i;
-// }
-// console.log(sum);
+for (let i = startNumber; i <= endNumber; i++) {
+  sum = sum + i;
+}
+console.log(sum);
 
 // 0+1 = 1
 // 1+2 = 3
@@ -205,27 +206,27 @@
 // Накопичити добуток (парних) чисел від 5 до 12 (6 8 10 12 = 5760)
 // 1*6*8*...
 
-// const startNumber = 5;
-// const END_NUM = 12;
+const startNumber = 5;
+const END_NUM = 12;
 
-// let dob = 1;
+let dob = 1;
 
-// for (let i = startNumber; i <= END_NUM; i++) {
-//   dob *= i;
-// }
+for (let i = startNumber; i <= END_NUM; i++) {
+  dob *= i;
+}
 
-// console.log(dob);
+console.log(dob);
 
-// const startNumber = 5;
-// const endNumber = 12;
-// let mul = 1;
+const startNumber = 5;
+const endNumber = 12;
+let mul = 1;
 
-// for (let i = startNumber; i <= endNumber; i++) {
-//   if (i % 2 === 0) {
-//     mul *= i;
-//   }
-// }
-// console.log("Mul:", mul);
+for (let i = startNumber; i <= endNumber; i++) {
+  if (i % 2 === 0) {
+    mul *= i;
+  }
+}
+console.log("Mul:", mul);
 
 // сформувати табличку з стовпчиками - датами від поточної до кінця місяця
 
@@ -261,7 +262,7 @@ if (
 } else {
   endDate = 28;
 }
-// 2
+// 2 спосіб
 // const currentYear = currentDate.getFullYear(); //2024
 // const nextMonth = monthNumber + 1;
 // const lastMonthDate = new Date(currentYear, nextMonth, 0);
