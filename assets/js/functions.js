0bfd37028808bea5b6704c6801a9835e66981368
Viktoriia <freshitpte@gmail.com>
2024-05-18 16:43:54 +0300

    Add summand ex

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -24,3 +24,24 @@ function sayHello(user, surname) {
 const helloString = sayHello("Test", "Testovych"); // "Hello Test Testovych"
 console.log("helloString :>> ", helloString);
 document.write(`<p>${helloString}</p>`);
+
+// Прописати функцію, яка повертає суму двох чисел
+function sum(a, b) {
+  return a + b;
+}
+
+function inputSummand(summandNumber) {
+  let summand;
+
+  do {
+    summand = prompt(`Enter ${summandNumber} number`);
+  } while (!Number.isFinite(+summand) || summand === "");
+
+  return Number(summand);
+}
+
+const firstSummand = inputSummand(1);
+const secondSummand = inputSummand(2);
+
+const result = sum(firstSummand, secondSummand);
+console.log(result);
