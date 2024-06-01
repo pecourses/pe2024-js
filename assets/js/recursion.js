4e7feea093d648dad1f1597d0c6f46f83f1d6a16
Viktoriia <freshitpte@gmail.com>
2024-06-01 20:03:46 +0300

    Add recursion

---
+++ assets/js/recursion.js
@@ -0,0 +1,29 @@
+// 0! = 1
+// 3! = 3 * 2 * 1 = 3 * 2! = 3 * 2 * 1! = 3 * 2 * 1 * 0! = 3 * 2 +* 1 * 1
+
+// n! = n * (n-1)!
+
+// !!! Обов'язково треба передбачити умову закінчення рекурсії
+
+function fact(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+
+const f = fact(50);
+
+// Вивести в консоль для bracketSeq(2) => (())
+//                       bracketSeq(4) => (((())))
+// (((()))) => ( (( () ))  )
+// bracketSeq(4) => ( bracketSeq(3) )
+
+function bracketSeq(n) {
+  if (n === 1) {
+    return "()";
+  }
+  return "(" + bracketSeq(n - 1) + ")";
+}
+console.log(bracketSeq(2));
+console.log(bracketSeq(4));
